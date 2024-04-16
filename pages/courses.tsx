52ed0b246608b69412680de5fc02ab2817097b05
--replace-all <pouryasoleimani93@gmail.com>
2024-04-16 08:31:37 -0700

    COMMITING MASTER CHANGES

--- pages/courses.tsx
+++ pages/courses.tsx
@@ -11,6 +11,7 @@ const Courses: NextPage<Props> = ({ }) => {
             <h2>HELLO WORLD</h2>
             <h3>THE TEST FOR commit -am</h3>
             <p>TESTING ALIASES</p>
+            <p>TESTING FOR MERGING NON-FAST-FORWARD BRANCHES</p>
         </div>
     )
 }
