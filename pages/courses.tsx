d9068b9f1cf5c8e957f11d7dd20c5926ca6eeb3d
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-18 08:42:34 -0700

    minimum changes

--- pages/courses.tsx
+++ pages/courses.tsx
@@ -4,6 +4,7 @@ interface Props { }
 
 const Courses: NextPage<Props> = ({ }) => {
     const age = 30
+    const setTicket = () => { return false }
     return (
         <div className='text-3xl font-extrabold flex flex-col space-y-4 p-6'>
             <h1>{age}</h1>
